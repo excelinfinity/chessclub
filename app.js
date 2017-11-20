be7fdba6eff8d22cbca7c49ecd81d3d2dbbbae8d
excelinfinity <vaibhavagrawaliitr@gmail.com>
2017-11-21 05:01:45 +0800

    remove unnessary logs

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
-
+app.use(logger('dev'));
 function checkLogin(req,res,next){
   var cookie = req.cookies;
   if(cookie && cookie.token){
